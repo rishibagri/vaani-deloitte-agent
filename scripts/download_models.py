eeda4eb1c8b067edeb26b620461187db28091f05
Rishi Bagri <your_email@purdue.edu>
2026-06-02 13:55:53 +0530

    Snapshot error

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -12,22 +12,22 @@ os.makedirs('../models/syncnet', exist_ok=True)
 os.makedirs('../models/face-parse-bisent', exist_ok=True)
 
 print('Downloading MuseTalk V1.0 weights...')
-snapshot_download('TMElyralab/MuseTalk', local_dir='../models', include=['musetalk/*'])
+snapshot_download('TMElyralab/MuseTalk', local_dir='../models', allow_patterns=['musetalk/*'])
 
 print('Downloading MuseTalk V1.5 weights...')
-snapshot_download('TMElyralab/MuseTalk', local_dir='../models', include=['musetalkV15/*'])
+snapshot_download('TMElyralab/MuseTalk', local_dir='../models', allow_patterns=['musetalkV15/*'])
 
 print('Downloading SD VAE...')
-snapshot_download('stabilityai/sd-vae-ft-mse', local_dir='../models/sd-vae', include=['config.json', 'diffusion_pytorch_model.bin'])
+snapshot_download('stabilityai/sd-vae-ft-mse', local_dir='../models/sd-vae', allow_patterns=['config.json', 'diffusion_pytorch_model.bin'])
 
 print('Downloading Whisper...')
-snapshot_download('openai/whisper-tiny', local_dir='../models/whisper', include=['config.json', 'pytorch_model.bin', 'preprocessor_config.json'])
+snapshot_download('openai/whisper-tiny', local_dir='../models/whisper', allow_patterns=['config.json', 'pytorch_model.bin', 'preprocessor_config.json'])
 
 print('Downloading DWPose...')
-snapshot_download('yzd-v/DWPose', local_dir='../models/dwpose', include=['dw-ll_ucoco_384.pth'])
+snapshot_download('yzd-v/DWPose', local_dir='../models/dwpose', allow_patterns=['dw-ll_ucoco_384.pth'])
 
 print('Downloading SyncNet...')
-snapshot_download('ByteDance/LatentSync', local_dir='../models/syncnet', include=['latentsync_syncnet.pt'])
+snapshot_download('ByteDance/LatentSync', local_dir='../models/syncnet', allow_patterns=['latentsync_syncnet.pt'])
 
 print('Downloading face parse model...')
 urllib.request.urlretrieve(
